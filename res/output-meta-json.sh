832c5b0fa7f2f7fc2d589b2be2a13e25a50a4095
Jan Haller <bromeon@gmail.com>
2023-03-25 18:10:45 +0100

    Automatic push

---
+++ res/output-meta-json.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+cat <<HEREDOC
+{
+    "protocol": 1,
+    "versions": {
+        "godot": "$GODOT4_VER",
+        "rust": "$RUST_VER",
+        "bindgen": "$BINDGEN_VER"
+    },
+    "created_ts": $(date +%s)
+}
+HEREDOC
