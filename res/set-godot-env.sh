916a64463c007e38e958f34b4745cbd32594dfe7
Jan Haller <bromeon@gmail.com>
2023-03-25 20:13:38 +0100

    Amendments to crate commit

--- res/set-godot-env.sh
+++ res/set-godot-env.sh
@@ -9,14 +9,17 @@ runnerTemp="$2"
 # if version has already suffix (e.g. -rc1), leave as-is, but replace filePath
 if [[ $version =~ "-" ]]; then
     filePath=$(echo "$version" | sed "s!-!/!")
+    wordyVersion="$version"
 else
     filePath="$version"
-    version="${version}-stable"
+    wordyVersion="${version}-stable"
 fi
 
-filename=Godot_v${version}_linux.x86_64
+filename=Godot_v${wordyVersion}_linux.x86_64
 
 echo "GODOT4_FILE_URL=$filePath/$filename.zip" >> $GITHUB_ENV
 echo "GODOT4_DIR=$runnerTemp/godot_bin" >> $GITHUB_ENV
 echo "GODOT4_BIN=$runnerTemp/godot_bin/$filename" >> $GITHUB_ENV
 echo "GODOT4_VER=$version" >> $GITHUB_ENV
+echo "GODOT4_WORDY_VER=$wordyVersion" >> $GITHUB_ENV
+
