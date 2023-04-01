2c8cd2abc675ad65045e47e2178fa240149de2f8
Jan Haller <bromeon@gmail.com>
2023-04-01 11:22:11 +0200

    Fix a few last-minute things
    Rename branch lz -> root, other cleanups

--- generator/build.rs
+++ generator/build.rs
@@ -7,21 +7,22 @@
 // This crate is just a shim around godot-bindings.
 // See also gdext's godot-ffi/build.rs which is similar in nature.
 
+// If you want to build this crate locally, you need to do two things from the repo root prior to build:
+//   cd .generated
+//   godot4 --headless --dump-gdextension-interface
+
 use std::path::Path;
 
 fn main() {
     let mut watch = godot_bindings::StopWatch::start();
 
     let gen_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../.generated"));
-    // let header_c_path = Path::new(env!("HEADER_C_PATH"));
-    // let header_c_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/gen/"));
     let header_c_path = gen_path.join("gdextension_interface.h");
     let header_rs_path = gen_path.join("gdextension_interface.rs");
 
-    // godot_bindings::clear_dir(gen_path, &mut watch);
-
     // Note: do not call clear_dir(), as we pass in the C header into the gen directory.
-    godot_bindings::write_gdextension_header_rs_from_c(&header_c_path, &header_rs_path, &mut watch);
+    //godot_bindings::clear_dir(gen_path, &mut watch);
+    godot_bindings::write_gdextension_headers_from_c(&header_c_path, &header_rs_path, &mut watch);
 
     watch.write_stats_to(&gen_path.join("godot-ffi-stats.txt"));
 }
