fd2cb73a75c6577b7c252a4f4357f4eb1d2896e0
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-06-23 05:29:11 +0000

    Godot  artifact sync

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,15 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-//! # Internal crate of [**godot-rust**](https://godot-rust.github.io)
-//!
-//! Do not depend on this crate directly, instead use the `godot` crate.
-//! No SemVer or other guarantees are provided.
+// Since this lib.rs is repurposed as a module file, avoid crate-level attributes such as
+// #![doc(html_logo_url)] here.
 
-#![doc(html_logo_url = "https://godotengine.org/assets/press/icon_color.svg")]
-
-// Note: this API is not necessarily forward-compatible. It can be updated on demand (for all published Godot versions).
-// Important is that the latest version of gdext can be used together with all latest published artifacts.
-// For long-term stability, it may be helpful to have a dynamic query mechanism, such as:
-//   fn get_property(key: &str) -> Option<String>
+//! # GDExtension API for Godot 4.3
 
 use std::borrow::Cow;
+
+/// Abstracts from borrow/owned and allows to change implementation without affecting API.
+pub type CowStr = Cow<'static, str>;
 
 /// Version of the Godot engine that the API JSON and C header mirror.
 ///
@@ -26,33 +22,31 @@ use std::borrow::Cow;
 pub const GODOT_VERSION_STRING: &str = "4.3";
 
 /// Returns the contents of the header file `gdextension_interface.h`.
-pub const fn load_gdextension_header_h() -> &'static str {
-    include_str!("../res/gdextension_interface.h")
+pub const fn load_gdextension_header_h() -> CowStr {
+    CowStr::Borrowed(include_str!("../res/gdextension_interface.h"))
 }
 
 /// Returns the contents of the header file `gdextension_interface.rs`, generated for the corresponding platform.
-pub const fn load_gdextension_header_rs() -> &'static str {
+pub const fn load_gdextension_header_rs() -> CowStr {
     #[cfg(windows)]
-    {
-        include_str!("../res/gdextension_interface_windows.rs")
-    }
+    let s = include_str!("../res/gdextension_interface_windows.rs");
+
     #[cfg(target_os = "macos")]
-    {
-        include_str!("../res/gdextension_interface_macos.rs")
-    }
+    let s = include_str!("../res/gdextension_interface_macos.rs");
+
     #[cfg(all(unix, not(target_os = "macos")))]
-    {
-        include_str!("../res/gdextension_interface_linux.rs")
-    }
+    let s = include_str!("../res/gdextension_interface_linux.rs");
+
+    CowStr::Borrowed(s)
 }
 
 /// Returns the contents of the JSON API file `extension_api.json`.
-pub const fn load_gdextension_json() -> &'static str {
-    include_str!("../res/extension_api.json")
+pub const fn load_gdextension_json() -> CowStr {
+    Cow::Borrowed(include_str!("../res/extension_api.json"))
 }
 
 /// Dynamically fetch a property of this crate.
-pub fn get_package_property(key: &str) -> Option<Cow<'static, str>> {
+pub fn get_package_property(key: &str) -> Option<CowStr> {
     let value = match key {
         "godot_version_string" => Cow::Borrowed(GODOT_VERSION_STRING),
         "rust_version_string" => Cow::Borrowed("1.79.0"),
