bc1ca14a6e612171025244364809ee426ea04282
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2025-03-12 05:32:02 +0000

    Godot  artifact sync

--- res/gdextension_interface_linux.rs
+++ res/gdextension_interface_linux.rs
@@ -4886,6 +4886,13 @@ pub type GDExtensionInterfaceObjectGetScriptInstance = ::std::option::Option<
         p_language: GDExtensionObjectPtr,
     ) -> GDExtensionScriptInstanceDataPtr,
 >;
+#[doc = " @name object_set_script_instance\n @since 4.5\n\n Set the script instance data attached to this object.\n\n @param p_object A pointer to the Object.\n @param p_script_instance A pointer to the script instance data to attach to this object."]
+pub type GDExtensionInterfaceObjectSetScriptInstance = ::std::option::Option<
+    unsafe extern "C" fn(
+        p_object: GDExtensionObjectPtr,
+        p_script_instance: GDExtensionScriptInstanceDataPtr,
+    ),
+>;
 #[doc = " @name callable_custom_create\n @since 4.2\n @deprecated in Godot 4.3. Use `callable_custom_create2` instead.\n\n Creates a custom Callable object from a function pointer.\n\n Provided struct can be safely freed once the function returns.\n\n @param r_callable A pointer that will receive the new Callable.\n @param p_callable_custom_info The info required to construct a Callable."]
 pub type GDExtensionInterfaceCallableCustomCreate = ::std::option::Option<
     unsafe extern "C" fn(
