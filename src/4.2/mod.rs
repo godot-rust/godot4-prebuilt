6dc66380c7e2c7f7b4e8f289904a5769eedbcec0
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2026-04-19 21:19:09 +0000

    Package gdextension-api v0.5.0

---
+++ src/4.2/mod.rs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) godot-rust; Bromeon and contributors.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+//! # GDExtension API for Godot 4.2
+
+use std::borrow::Cow;
+
+/// Abstracts from borrow/owned and allows to change implementation without affecting API.
+pub type CowStr = Cow<'static, str>;
+
+/// Version of the Godot engine that the API JSON and C header mirror.
+///
+/// Note that this currently only contains the `major.minor[.patch]` part, so even `4.2-rc1` would be `4.2` (although pre-releases are currently
+/// not published).
+pub const GODOT_VERSION_STRING: &str = "4.2";
+
+/// Returns the contents of the JSON API file `extension_api.json`.
+pub const fn load_gdextension_json() -> CowStr {
+    Cow::Borrowed(include_str!("extension_api.json"))
+}
+
+/// Returns the contents of `gdextension_interface.json`.
+///
+/// Describes the GDExtension C API. Backwards-compatible with all supported Godot versions.
+pub const fn load_gdextension_interface_json() -> CowStr {
+    Cow::Borrowed(include_str!("../gdextension_interface.json"))
+}
+
+/// Dynamically fetch a property of this crate.
+pub fn get_package_property(key: &str) -> Option<CowStr> {
+    let value = match key {
+        "godot_version_string" => Cow::Borrowed(GODOT_VERSION_STRING),
+        _ => return None,
+    };
+
+    Some(value)
+}
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! # Godot 4 GDExtension API, mapped to Rust.
+//!
+//! Used internally by the [godot-rust](https://godot-rust.github.io) project.
+//! We do not offer any guarantees for the provided API.
+
+#![doc(html_logo_url = "https://godotengine.org/assets/press/icon_color.svg")]
+#[path = "4.2/mod.rs"]
+pub mod version_4_2;
+#[path = "4.3/mod.rs"]
+pub mod version_4_3;
+#[path = "4.4/mod.rs"]
+pub mod version_4_4;
+#[path = "4.5/mod.rs"]
+pub mod version_4_5;
+#[path = "4.6/mod.rs"]
+pub mod version_4_6;
